87fc4494a1b21a40cc50e59fb6fb98dc46422e20
pharma999 <pharma999999999@gmail.com>
2025-12-27 15:01:23 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,33 +1,59 @@
+// package main
+
+// import (
+//  "os"
+//  "log"
+//  "github.com/gin-gonic/gin"
+//  //"go.mongodb.org/mongo-driver/v2/mongo"
+//  //"github.com/pharma999/vender/database"
+//  "github.com/pharma999/vender/routes"
+//  //"github.com/pharma999/vender/controller"
+//  "github.com/joho/godotenv"
+
+// )
+
+// //var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "vender")
+
+// func main() {
+// 	//database.ConnectDB()
+// 	err := godotenv.Load()
+//   	if err != nil {
+//     	log.Fatal("Error loading .env file")
+//     }
+// 	port := os.Getenv("PORT")
+
+// 	if port == ""{
+// 		port = "8000"
+// 	}
+
+	
+    
+
+  
+    
+
+// 	router := gin.New()
+// 	router.Use(gin.Logger())
+// 	routes.VenderRoutes(router)
+// 	routes.VenderDataRoutes(router)
+// 	routes.ProductDetailRouter(router)
+
+
+
 package main
 
 import (
- "os"
- "log"
- "github.com/gin-gonic/gin"
- //"go.mongodb.org/mongo-driver/v2/mongo"
- //"github.com/pharma999/vender/database"
- "github.com/pharma999/vender/routes"
- //"github.com/pharma999/vender/controller"
- "github.com/joho/godotenv"
+    "os"
+    "log"
 
+    "github.com/gin-gonic/gin"
+    "github.com/pharma999/vender/routes"
+    "github.com/joho/godotenv"
 )
 
-//var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "vender")
-
 func main() {
-	//database.ConnectDB()
-	#err := godotenv.Load()
-  	#if err != nil {
-    #	log.Fatal("Error loading .env file")
-   # }
-	#port := os.Getenv("PORT")
 
-	#if port == ""{
-		#port = "8000"
-	#}
-
-	
-    // .env sirf local ke liye
+    // Load .env only if present (safe for all clouds)
     _ = godotenv.Load()
 
     port := os.Getenv("PORT")
@@ -35,15 +61,17 @@ func main() {
         port = "8000"
     }
 
-  
-    
+    router := gin.New()
+    router.Use(gin.Logger())
 
-	router := gin.New()
-	router.Use(gin.Logger())
-	routes.VenderRoutes(router)
-	routes.VenderDataRoutes(router)
-	routes.ProductDetailRouter(router)
+    routes.VenderRoutes(router)
+    routes.VenderDataRoutes(router)
+    routes.ProductDetailRouter(router)
 
-
-	router.Run(":" + port)
+    log.Println("Server running on port", port)
+    router.Run(":" + port)
 }
+
+
+// 	router.Run(":" + port)
+// }
